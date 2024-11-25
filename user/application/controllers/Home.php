c8a3e73179f036fda95f11f83d54ab234b27e07f
Native Muttaqien <muttaqien0111@gmail.com>
2024-11-25 23:02:41 +0700

    Tambah user

---
+++ user/application/controllers/Home.php
@@ -0,0 +1,13 @@
+<?php 
+class Home extends CI_Controller {
+
+	function index() {
+
+		$this->load->view('header');
+        $this->load->view('home');	
+		$this->load->view('footer');
+		$this->load->helper('url');
+
+	}
+}
+ ?>
---
+++ user/application/controllers/Services.php
@@ -0,0 +1,14 @@
+<?php 
+class Services extends CI_Controller {
+
+	function index() {
+
+		$this->load->view('header');
+        $this->load->view('services');	
+		$this->load->view('footer');
+		$this->load->helper('url');
+
+	}
+    
+}
+ ?>
