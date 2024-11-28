7c6011eb604bd8efb15fe0cdec7f41211793ffa3
Native Muttaqien <muttaqien0111@gmail.com>
2024-11-28 11:59:56 +0700

    Penggunaaan template, tetapi main content masih tertutup side navigation

--- admin/application/views/header.php
+++ admin/application/views/header.php
@@ -1,103 +1,29 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin BersihinAja</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js"></script>
-
-
-  <style>
-    body {
-      background-color: #fbfbfb;
-    }
-
-    main {
-      margin-top: 60px;
-      margin-left: 60px;
-      transition: margin-left 0.3s ease-in-out;
-    }
-
-    .sidebar {
-      position: fixed;
-      top: 0;
-      bottom: 0;
-      left: 0;
-      width: 240px;
-      background-color: #fff;
-      box-shadow: 0 2px 5px 0 rgb(0 0 0 / 5%), 0 2px 10px 0 rgb(0 0 0 / 5%);
-      z-index: 600;
-      transition: transform 0.3s ease-in-out;
-      transform: translateX(-240px); /* Awal tersembunyi */
-    }
-
-    .sidebar.show {
-      transform: translateX(0); /* Ditampilkan */
-    }
-
-    .sidebar.collapsed {
-      transform: translateX(-240px); /* Tersembunyi */
-    }
-
-    .sidebar .active {
-      border-radius: 5px;
-      box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
-    }
-
-    .sidebar-sticky {
-      position: relative;
-      top: 0;
-      height: calc(100vh - 48px);
-      padding-top: 0.5rem;
-      overflow-x: hidden;
-      overflow-y: auto;
-    }
-
-    @media (min-width: 992px) {
-      .sidebar {
-        transform: translateX(0); /* Selalu terlihat pada layar lebar */
-      }
-
-      .sidebar.collapsed {
-        transform: translateX(0); /* Abaikan collapsed pada layar lebar */
-      }
-
-      main {
-        margin-left: 240px; /* Konten utama bergeser sesuai lebar sidebar */
-      }
-
-      .sidebar.show ~ main {
-        margin-left: 240px;
-      }
-    }
-
-    @media (max-width: 991.98px) {
-      .sidebar {
-        transform: translateX(-100%); /* Tersembunyi pada layar kecil */
-      }
-
-      .sidebar.show {
-        transform: translateX(0); /* Ditampilkan pada layar kecil */
-      }
-
-      main {
-        margin-left: 0; /* Konten tidak bergeser saat sidebar tersembunyi */
-      }
-    }
-  </style>
-
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>Material Design for Bootstrap</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v6.0.0/css/all.css" />
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" />
+  <!-- MDB -->
+  <link rel="stylesheet" href="<?php echo base_url('/assets/css/mdb.min.css'); ?>" />
+  <link rel="stylesheet" href="<?php echo base_url('/assets/css/admin.css'); ?>" />
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.min.js" integrity="sha512-d9xgZrVZpmmQlfonhQUvTR7lMPtO7NkZMkA0ABN3PHCbKA5nqylQ/yWlFAyY6hYgdF1Qh6nYiuADWwKB4C2WSw=="
+    crossorigin="anonymous"></script>
 </head>
 
 <body>
-
-  <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
-    <div class="position-sticky">
-      <div class="list-group list-group-flush mx-3 mt-4">
-
+  <!--Main Navigation-->
+  <header>
+    <!-- Sidebar -->
+    <nav id="sidebarMenu" class="collapse d-lg-block sidebar collapse bg-white">
+      <div class="position-sticky">
+        <div class="list-group list-group-flush mx-3 mt-4">
         <a href="<?php echo base_url("home") ?>" class="list-group-item list-group-item-action py-2 ripple" aria-current="true">
           <i class="fas fa-tachometer-alt fa-fw me-3"></i><span>Home</span>
         </a>
@@ -125,82 +51,141 @@
         <a href="<?php echo base_url("transaksi") ?>" class="list-group-item list-group-item-action py-2 ripple"><i class="fas fa-globe fa-fw me-3">
           </i><span>Transaksi</span>
         </a>
-
+        </div>
       </div>
-    </div>
-  </nav>
-  <!-- Sidebar -->
+    </nav>
+    <!-- Sidebar -->
 
-  <!-- Navbar -->
-  <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-    <!-- Container wrapper -->
-    <div class="container-fluid">
-      <!-- Toggle button -->
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#sidebarMenu"
-        aria-controls="sidebarMenu"
-        aria-expanded="false"
-        aria-label="Toggle navigation">
-        <i class="fas fa-bars"></i>
-      </button>
+    <!-- Navbar -->
+    <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
+      <!-- Container wrapper -->
+      <div class="container-fluid">
+        <!-- Toggle button -->
+        <button class="navbar-toggler" type="button" data-mdb-collapse-init data-mdb-target="#sidebarMenu"
+          aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <i class="fas fa-bars"></i>
+        </button>
 
-      <!-- Brand -->
-    
-      <a class="navbar-brand px-4" href="#">
-          <span class="text-2xl font-bold text-gray-800 flex items-center">
-            <img 
-            src="https://cdn.discordapp.com/attachments/962882027093364826/1311305244285599816/wind.svg?ex=67485fad&is=67470e2d&hm=87b066648a3b4132893b9c95f6a1821225b3685e654603a3dc4cbea7cfb18212&"
-            alt="logo" 
-            class="h-6 mr-2"
-            height="25"
-            alt=""
-            loading="lazy">
-            BersihinAja
-          </span>
-      </a>
-      <!-- Search form -->
-      <form class="d-none d-md-flex input-group w-auto my-auto">
-        <input
-          autocomplete="off"
-          type="search"
-          class="form-control rounded"
-          placeholder='Search (ctrl + "/" to focus)'
-          style="min-width: 225px" />
-        <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
-      </form>
+        <!-- Brand -->
+        <a class="navbar-brand" href="#">
+          <img src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png" height="25" alt="" loading="lazy" />
+        </a>
+        <!-- Search form -->
+        <form class="d-none d-md-flex input-group w-auto my-auto">
+          <input autocomplete="off" type="search" class="form-control rounded"
+            placeholder='Search (ctrl + "/" to focus)' style="min-width: 225px" />
+          <span class="input-group-text border-0"><i class="fas fa-search"></i></span>
+        </form>
 
-      <!-- Right links -->
-      <ul class="navbar-nav ms-auto d-flex flex-row">
+        <!-- Right links -->
+        <ul class="navbar-nav ms-auto d-flex flex-row">
+          <!-- Notification dropdown -->
+          <li class="nav-item dropdown">
+            <a class="nav-link me-3 me-lg-0 dropdown-toggle hidden-arrow" href="#" id="navbarDropdownMenuLink"
+              role="button" data-mdb-dropdown-init aria-expanded="false">
+              <i class="fas fa-bell"></i>
+              <span class="badge rounded-pill badge-notification bg-danger">1</span>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">Some news</a></li>
+              <li><a class="dropdown-item" href="#">Another news</a></li>
+              <li>
+                <a class="dropdown-item" href="#">Something else</a>
+              </li>
+            </ul>
+          </li>
 
-        <!-- Icon -->
-        <li class="nav-item me-3 me-lg-0">
-          <a class="nav-link" href="#">
-            <i class="fab fa-github"></i>
-          </a>
-        </li>
+          <!-- Icon -->
+          <li class="nav-item">
+            <a class="nav-link me-3 me-lg-0" href="#">
+              <i class="fas fa-fill-drip"></i>
+            </a>
+          </li>
+          <!-- Icon -->
+          <li class="nav-item me-3 me-lg-0">
+            <a class="nav-link" href="#">
+              <i class="fab fa-github"></i>
+            </a>
+          </li>
 
+          <!-- Icon dropdown -->
+          <li class="nav-item dropdown">
+            <a class="nav-link me-3 me-lg-0 dropdown-toggle hidden-arrow" href="#" id="navbarDropdown" role="button"
+              data-mdb-dropdown-init aria-expanded="false">
+              <i class="united kingdom flag m-0"></i>
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+              <li>
+                <a class="dropdown-item" href="#"><i class="united kingdom flag"></i>English
+                  <i class="fa fa-check text-success ms-2"></i></a>
+              </li>
+              <li>
+                <hr class="dropdown-divider" />
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="poland flag"></i>Polski</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="china flag"></i>中文</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="japan flag"></i>日本語</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="germany flag"></i>Deutsch</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="france flag"></i>Français</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="spain flag"></i>Español</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="russia flag"></i>Русский</a>
+              </li>
+              <li>
+                <a class="dropdown-item" href="#"><i class="portugal flag"></i>Português</a>
+              </li>
+            </ul>
+          </li>
 
-        <!-- Avatar -->
-        <li class="nav-item">
-                <a href="<?php echo base_url("akun") ?>" class="nav-link">
-                    <?php echo $this->session->userdata("nama") ?>       
-                </a>
-        </li>
-      </ul>
-    </div>
-    <!-- Container wrapper -->
-      </nav>
-    <!--Main layout-->
-    <main style="margin-top: 58px">
-      <div class="container pt-4">
-
+          <!-- Avatar -->
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle hidden-arrow d-flex align-items-center" href="#"
+              id="navbarDropdownMenuLink" role="button" data-mdb-dropdown-init aria-expanded="false">
+              <img src="https://mdbootstrap.com/img/Photos/Avatars/img (31).jpg" class="rounded-circle" height="22"
+                alt="" loading="lazy" />
+            </a>
+            <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+              <li><a class="dropdown-item" href="#">My profile</a></li>
+              <li><a class="dropdown-item" href="#">Settings</a></li>
+              <li><a class="dropdown-item" href="#">Logout</a></li>
+            </ul>
+          </li>
+        </ul>
       </div>
-    </main>
+      <!-- Container wrapper -->
+    </nav>
+    <!-- Navbar -->
+  </header>
+  <!--Main Navigation-->
 
+  <!--Main layout-->
+  <main style="margin-top: 58px">
+    <div class="container pt-4">
+      <!-- Section: Main chart -->
+      <section class="mb-4">
 
-  </body>
+      </section>
+      <!-- Section: Main chart -->
+
+    </div>
+  </main>
+  <!--Main layout-->
+  <!-- MDB -->
+  <script src="<?php echo base_url('/assets/js/mdb.umd.min.js'); ?>"></script>
+  <script src="<?php echo base_url('/assets/js/admin.js'); ?>"></script>
+
+</body>
 
 </html>
